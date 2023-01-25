4920f528a2b450a53bdacdec28e530555924fafe
marim694 <marim694@su17-117.ad.liu.se>
2023-01-25 08:44:03 +0100

    Copy C++ code from lab page

--- air_lab2/src/random_exploration.cpp
+++ air_lab2/src/random_exploration.cpp
@@ -3,125 +3,115 @@
 #include <memory>
 #include <string>
 #include <sstream>
-#include <cmath>
+
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
 
-using namespace std::placeholders;
-
-namespace action_tutorials_cpp
+class RandomExploration : public rclcpp::Node
 {
-  class FibonacciActionClient : public rclcpp::Node
-  {
-    public:
-      using Fibonacci = nav2_msgs::action::NavigateToPose;
-      using GoalHandleFibonacci = rclcpp_action::ClientGoalHandle<Fibonacci>;
+  public:
+    using NavigateToPose = nav2_msgs::action::NavigateToPose;
+    using GoalHandleNavigateToPose = rclcpp_action::ClientGoalHandle<NavigateToPose>;
 
-      explicit FibonacciActionClient()
-        : Node("fibonacci_action_client")
-      {
-        this->client_ptr_ = rclcpp_action::create_client<Fibonacci>(
-            this,
-            "fibonacci"
-            );
+    explicit RandomExploration()
+      : Node("random_exploration")
+    {
+      this->client_ptr_ = rclcpp_action::create_client<NavigateToPose>(
+	  this,
+	  "navigate_to_pose");
+
+      this->timer_ = this->create_wall_timer(
+	  std::chrono::milliseconds(500),
+	  std::bind(&RandomExploration::send_goal, this));
+    }
+
+    void send_goal()
+    {
+      double x = 10.0;
+      double y = 5.0;
+      double angle = 1.0;
+
+      auto goal_msg = NavigateToPose::Goal();
+      goal_msg.pose.header.frame_id = "map";
+      goal_msg.pose.pose.position.x = x;
+      goal_msg.pose.pose.position.y = y;
+      goal_msg.pose.pose.orientation.w = std::cos(angle/2);
+      goal_msg.pose.pose.orientation.z = std::sin(angle/2);
+
+      using namespace std::placeholders;
+
+      this->timer_->cancel();
+
+      if (!this->client_ptr_->wait_for_action_server()) {
+	RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+	rclcpp::shutdown();
       }
 
-      void send_goal(double x, double y, double angle)
-      {
-        if (!this->client_ptr_->wait_for_action_server()) {
-          RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
-          rclcpp::shutdown();
+      RCLCPP_INFO(this->get_logger(), "Sending goal");
 
-        }
+      auto send_goal_options = rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
+      send_goal_options.goal_response_callback =
+	std::bind(&RandomExploration::goal_response_callback, this, _1);
+      send_goal_options.feedback_callback =
+	std::bind(&RandomExploration::feedback_callback, this, _1, _2);
+      send_goal_options.result_callback =
+	std::bind(&RandomExploration::result_callback, this, _1);
+      this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+    }
 
-        auto goal_msg = Fibonacci::Goal();
-        goal_msg.pose.header.frame_id = "map";
-        goal_msg.pose.pose.position.x = x;
-        goal_msg.pose.pose.position.y = y;
-        goal_msg.pose.pose.orientation.w = std::cos(angle/2);
-        goal_msg.pose.pose.orientation.z = std::sin(angle/2);
+  private:
+    rclcpp_action::Client<NavigateToPose>::SharedPtr client_ptr_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
-        RCLCPP_INFO(this->get_logger(), "Sending goal");
-
-        auto send_goal_options = rclcpp_action::Client<Fibonacci>::SendGoalOptions();
-        send_goal_options.goal_response_callback =
-          std::bind(&FibonacciActionClient::goal_response_callback, this, _1);
-        send_goal_options.feedback_callback =
-          std::bind(&FibonacciActionClient::feedback_callback, this, _1, _2);
-        send_goal_options.result_callback =
-          std::bind(&FibonacciActionClient::result_callback, this, _1);
-        this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
-
+    void goal_response_callback(std::shared_future<GoalHandleNavigateToPose::SharedPtr> future)
+    {
+      auto goal_handle = future.get();
+      if (!goal_handle) {
+	RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+      } else {
+	RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
       }
+    }
 
-    private:
-      rclcpp_action::Client<Fibonacci>::SharedPtr client_ptr_;
-
-      void goal_response_callback(std::shared_future<GoalHandleFibonacci::SharedPtr> future)
+    void feedback_callback(
+	GoalHandleNavigateToPose::SharedPtr goal_handle,
+	const std::shared_ptr<const NavigateToPose::Feedback> feedback)
+    {
+      if(feedback->navigation_time.sec>60)
       {
-        auto goal_handle = future.get();
-        if (!goal_handle) {
-          RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
-
-        } else {
-          RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
-
-        }
-        //TODO copy the thing and then maybe we don't need it
-        //        self._get_result_future = goal_handle.get_result_async()
-        //                self._get_result_future.add_done_callback(self.get_result_callback)
+	client_ptr_->async_cancel_goal(goal_handle);
+	RCLCPP_INFO(this->get_logger(), "Timeout");
+	rclcpp::shutdown();
       }
+    }
 
-      void feedback_callback(
-          GoalHandleFibonacci::SharedPtr handle,
-          const std::shared_ptr<const Fibonacci::Feedback> feedback
-          )
-      {
-        std::stringstream ss;
-        ss << "Received feedback: " << feedback->navigation_time.sec << std::endl;
-        RCLCPP_INFO(this->get_logger(), ss.str().c_str());
-        if(feedback->navigation_time.sec > 60) {
-          client_ptr_->async_cancel_goal(handle, [this](auto) {
-            RCLCPP_INFO(this->get_logger(), "Testor");
-          });
-          rclcpp::shutdown();
-        }
+    void result_callback(const GoalHandleNavigateToPose::WrappedResult & result)
+    {
+      switch (result.code) {
+	case rclcpp_action::ResultCode::SUCCEEDED:
+	  RCLCPP_INFO(this->get_logger(), "Goal was succeeded");
+	  break;
+	case rclcpp_action::ResultCode::ABORTED:
+	  RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+	  return;
+	case rclcpp_action::ResultCode::CANCELED:
+	  RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+	  return;
+	default:
+	  RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+	  return;
       }
+      rclcpp::shutdown();
+    }
+};  // class RandomExploration
 
-      void result_callback(const GoalHandleFibonacci::WrappedResult & result)
-      {
-        switch (result.code) {
-          case rclcpp_action::ResultCode::SUCCEEDED:
-            break;
-          case rclcpp_action::ResultCode::ABORTED:
-            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
-            return;
-          case rclcpp_action::ResultCode::CANCELED:
-            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
-            return;
-          default:
-            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
-            return;
-
-        }
-        rclcpp::shutdown();
-      }
-
-  };  // class FibonacciActionClient
-
-
-}  // namespace action_tutorials_cpp
-using action_tutorials_cpp::FibonacciActionClient;
-
-int main(int argc, char** argv) {
-
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
-  std::shared_ptr<FibonacciActionClient> fibo{};
-  rclcpp::spin(fibo);
-  fibo->send_goal(10, 5, 1);
-
+  rclcpp::spin(std::make_shared<RandomExploration>());
   rclcpp::shutdown();
+  return 0;
 }
+
