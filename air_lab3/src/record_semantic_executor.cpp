fb1ed1a950b85c4ef859c56b53836c3cdf1c4a10
marim694 <marim694@tlvm-4-2-3.ad.liu.se>
2023-02-17 15:33:56 +0100

    Lab4 initial

--- air_lab3/src/record_semantic_executor.cpp
+++ air_lab3/src/record_semantic_executor.cpp
@@ -1,6 +1,9 @@
+#include <TstML/Executor/ExecutionStatus.h>
 #include <functional>
 #include <future>
 #include <memory>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/future_return_code.hpp>
 #include <sstream>
 #include <string>
 #include <thread>
@@ -8,12 +11,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-#include "air_simple_sim_msgs/msg/SemanticObservations.hpp"
+#include "air_simple_sim_msgs/msg/semantic_observation.hpp"
+#include "ros2_kdb_msgs/srv/query_database.hpp"
 
 #include <TstML/Executor/AbstractNodeExecutor.h>
 #include <TstML/TSTNode.h>
 
-using air_simple_sim_msgs::msg::SemanticObservations;
+using air_simple_sim_msgs::msg::SemanticObservation;
+using ServiceT = ros2_kdb_msgs::srv::QueryDatabase;
 
 class RecordSemanticExecutor : public TstML::Executor::AbstractNodeExecutor {
 public:
@@ -24,16 +29,52 @@ public:
     m_node = rclcpp::Node::make_shared("_node" + std::to_string(++counter));
     m_executor.add_node(m_node);
     m_executor_thread = std::thread([this]() { m_executor.spin(); });
+  }
 
-    std::string topic{node()->getParameter(TsTML::TSTNode::ParameterType::Specific, "topic").to_string()};
-    std::string graphname{node()->getParameter(TsTML::TSTNode::ParameterType::Specific, "graphname").to_string()};
+  TstML::Executor::ExecutionStatus start() override {
+    std::string topic{
+        node()
+            ->getParameter(TstML::TSTNode::ParameterType::Specific, "topic")
+            .toString()
+            .toStdString()};
+    graph_name =
+        node()
+            ->getParameter(TstML::TSTNode::ParameterType::Specific, "graphname")
+            .toString()
+            .toStdString();
 
-    subscription = m_node.create_subscription<SemanticObservations>( topic, 10);
+    subscription = m_node->create_subscription<SemanticObservation>(
+        topic, 10,
+        std::bind(&RecordSemanticExecutor::semantic_callback, this,
+                  std::placeholders::_1));
+
+    client = m_node->create_client<ServiceT>("/kdb_server/sparql_query");
+    return TstML::Executor::ExecutionStatus::Started();
   }
 
   ~RecordSemanticExecutor() {
     m_executor.cancel();
     m_executor_thread.join();
+  }
+
+  void semantic_callback(const SemanticObservation::SharedPtr msg) {
+    RCLCPP_INFO(this->m_node->get_logger(), "I heard:\n%f", msg->point.point.x);
+    rclcpp::Client<ServiceT>::SharedRequest request;
+    request->graphname = graph_name;
+    request->query =
+        "PREFIX gis: <http://www.ida.liu.se/~TDDE05/gis>"
+        "PREFIX properties: <http://www.ida.liu.se/~TDDE05/properties>"
+        "SELECT ?x ?y WHERE { <" +
+        msg->uuid + "> a <" + msg->klass +
+        "> ;"
+        "properties:location [ gis:x ?x; gis:y ?y ] . }";
+    auto response = client->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(m_node, response) ==
+        rclcpp::FutureReturnCode ::SUCCESS) {
+      RCLCPP_INFO(this->m_node->get_logger(), "Success");
+    } else {
+      RCLCPP_INFO(this->m_node->get_logger(), "Failure");
+    }
   }
 
   TstML::Executor::ExecutionStatus pause() override {
@@ -43,11 +84,9 @@ public:
     return TstML::Executor::ExecutionStatus::Running();
   }
   TstML::Executor::ExecutionStatus stop() override {
-    m_client_ptr->async_cancel_goal(m_goal_handle);
     return TstML::Executor::ExecutionStatus::Finished();
   }
   TstML::Executor::ExecutionStatus abort() override {
-    m_client_ptr->async_cancel_goal(m_goal_handle);
     return TstML::Executor::ExecutionStatus::Aborted();
   }
 
@@ -55,5 +94,7 @@ private:
   std::shared_ptr<rclcpp::Node> m_node;
   rclcpp::executors::MultiThreadedExecutor m_executor;
   std::thread m_executor_thread;
-  rclcpp::Subscription<SemanticObservations>::SharedPtr subscription;
+  rclcpp::Subscription<SemanticObservation>::SharedPtr subscription;
+  std::string graph_name;
+  rclcpp::Client<ServiceT>::SharedPtr client;
 };
